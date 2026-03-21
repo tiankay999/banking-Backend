a49a83664c3ec0ef5cd37507202e1f1a697385fc
tiankay999 <tiankay999@gmail.com>
2026-03-21 20:23:27 +0000

    updated the authmiddle ware code base in cookies

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,38 +1,23 @@
 const jwt=  require ("jsonwebtoken");
 
 module.exports = authMiddleware= ( req,res,next)=>{
+  const token = req.cookies.token;
 
+  if (!token) {
+    return res.status(401).json({ message: "Not authorized" });
+  }
 
-
-try{
- const authHeader = req.headers.authorization;
-
- if(!authHeader ||!authHeader.startsWith("Bearer") )
-{
-    return res.status(401).json({message:"No token Provided"});
-
-}
-
-const token = authHeader.split(" ")[1];
-
-const decoded= jwt.verify(token,process.env.JWT_SECRET);
-
-
-
-req.user=decoded;
-
-next();
-
-
-
-} catch(err){
-    return res.status(401).json({message:"Invalid Token"})
-}
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
 
 
 
 
 
 
-
-}
